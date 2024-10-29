133548b0c922ee59cb115073ef0913784cab4d4a
zainababbasi-11 <zainabkhalidabbasi@outlook.com>
2024-10-29 17:24:47 +0500

    updateagainby29oct

--- cypress/Pages/DxloginPage.js
+++ cypress/Pages/DxloginPage.js
@@ -27,7 +27,7 @@ class Login {
   
     clickLogOut() {
       cy.get("button.MuiButtonBase-root").click({ force: true });
-      cy.wait(100);
+      //cy.wait(100);
       cy.get("li.MuiButtonBase-root").eq(1).click({ force: true });
       //cy.wait(200);
     }
